40155b8b7b864e4642f3712d84d5881edc96a493
ichristine180 <ichristine180@gmail.com>
2025-09-22 10:29:31 +0200

    script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import Script from "next/script";
 import "./globals.css";
 import { AppProvider } from "@/context/AppContext";
 
-
-
 // ROOT LAYOUT
 export default function RootLayout({
   children,
@@ -14,15 +12,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       
-
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EK39Z785QR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EK39Z785QR');
+          `}
+        </Script>
       </head>
       <body suppressHydrationWarning={true}>
         <AppProvider>{children}</AppProvider>
-
-       
-
-     
       </body>
     </html>
   );
